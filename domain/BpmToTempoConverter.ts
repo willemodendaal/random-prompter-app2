ad6e22619b40651fc008076aa76d303f8436c121
willem <willem.odendaal@gmail.com>
2024-02-20 14:43:06 +0200

    Add logic to convert bpm to delay

--- domain/BpmToTempoConverter.ts
+++ domain/BpmToTempoConverter.ts
@@ -1,3 +1,11 @@
-export function convertBpmToDelayMs(bpm: number) {
-    return bpm;
+export function convertBpmToDelayMs(bpm: number) : number {
+    const aBeatEveryXSeconds = 60 / bpm;
+    return Math.trunc(aBeatEveryXSeconds * 1000);
+}
+
+export function convertDelayMsToBpm(delayMs: number) : number {
+    const unrounded = 60 / (delayMs / 1000);
+
+    // Round result to 1 decimal.
+    return Math.round(unrounded * 10) / 10;
 }
--- domain/__tests__/BpmToTempoConverter-test.ts
+++ domain/__tests__/BpmToTempoConverter-test.ts
@@ -1,6 +1,60 @@
-describe('BlockCalculator.lengthBetweenIndexes', () => {
+import { convertBpmToDelayMs, convertDelayMsToBpm } from "../BpmToTempoConverter";
 
-    it('various scenarios', () => {
-        expect(1).toBe(1);
+describe('BpmToTempoConverter, bpm to delay', () => {
+
+    it('bpm to delayMs 1s', () => {
+        const delayMs = convertBpmToDelayMs(1);
+
+        // 1 bpm means there is a delay of 60 seconds between beats.
+        // (there is only one per minute).
+        // (converted to milliseconds by x100)
+        expect(delayMs).toBe(60 * 1000);
+    });
+
+    it('2 bpm to delayMs', () => {
+        const delayMs = convertBpmToDelayMs(2);
+        expect(delayMs).toBe(60 * 500);
+    });
+
+    it('60 bpm to delayMs', () => {
+        const delayMs = convertBpmToDelayMs(60);
+        expect(delayMs).toEqual(1000);
+    });
+
+    it('delayMs result is truncated (no need for sub-millisecond results)', () => {
+        const delayMs = convertBpmToDelayMs(211);
+        expect(delayMs).toEqual(284);
+    });
+});
+
+describe('BpmToTempoConverter, delay to bpm', () => {
+    it('delayMs 60 seconds is 1bpm', () => {
+        const bpm = convertDelayMsToBpm(60 * 1000);
+        expect(bpm).toBe(1);
+    });
+
+    it('delayMs 30 seconds is 2bpm', () => {
+        const bpm = convertDelayMsToBpm(30 * 1000);
+        expect(bpm).toBe(2);
+    });
+
+    it('delayMs 120 seconds is 0.5bpm', () => {
+        const bpm = convertDelayMsToBpm(120 * 1000);
+        expect(bpm).toBe(0.5);
+    });
+
+    it('delayMs 1 second is 60bpm', () => {
+        const bpm = convertDelayMsToBpm(1 * 1000);
+        expect(bpm).toBe(60);
+    });
+
+    it('delayMs 0.5 second is 60bpm', () => {
+        const bpm = convertDelayMsToBpm(500);
+        expect(bpm).toBe(120);
+    });
+
+    it('Bpm result is rounded to 1 decimal', () => {
+        const bpm = convertDelayMsToBpm(511);
+        expect(bpm).toBe(117.4);
     });
 });
