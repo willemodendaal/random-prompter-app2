9da5116ec2389b0b35462295585a2dc86d104d87
willem <willem.odendaal@gmail.com>
2024-02-20 20:27:31 +0200

    WIP tempo validation

--- components/TempoChooser.tsx
+++ components/TempoChooser.tsx
@@ -2,30 +2,81 @@ import { useEffect, useState } from "react";
 import { StyleSheet, Text, TextInput, View } from 'react-native';
 import { convertBpmToDelayMs, convertDelayMsToBpm } from '../domain/BpmToTempoConverter';
 
+const MaxDelaySeconds = 60 * 60; // 60 minutes, seems like a more than reasonable max.
+const MaxBpm = 1000;  // Every 30ms. Good enough.
+
 function delayMsToDelaySeconds(delayMs: number) {
     return delayMs / 1000;
+}
+
+function isValidDelaySeconds(newDelaySecondsStr: string) {
+    if (newDelaySecondsStr.trim() === "") {
+        return false;
+    }
+
+    const delaySeconds = Number(newDelaySecondsStr);
+    if (delaySeconds > 0 && delaySeconds < MaxDelaySeconds) {
+        return true;
+    }
+    else {
+        return false;
+    };
+}
+
+function isValidBpm(newBpmStr: string) {
+    if (newBpmStr.trim() === "") {
+        return false;
+    }
+
+    const bpm = Number(newBpmStr);
+    if (bpm > 0 && bpm < MaxBpm) {
+        return true;
+    }
+    else {
+        return false;
+    };
 }
 
 export default function TempoChooser({ onTempoChanged, initialValue }
     : { onTempoChanged: (newVal: number) => void, initialValue: number }) {
     const [bpmStr, setBpmStr] = useState("" + initialValue);
+    const [isValid, setIsValid] = useState(true);
     const [delaySecondsStr, setDelaySecondsStr] = useState("" + delayMsToDelaySeconds(convertBpmToDelayMs(initialValue)));
 
     const onBpmChanged = (newBpmStr: string) => {
+        if (!isValidBpm(newBpmStr)) {
+            setIsValid(false);
+            setBpmStr(newBpmStr);
+            return;
+        }
+
         const bpm = Number(newBpmStr);
         setBpmStr(newBpmStr);
         setDelaySecondsStr("" + delayMsToDelaySeconds(convertBpmToDelayMs(bpm)));
-        onTempoChanged(bpm);
+
+        if (isValid) {
+            onTempoChanged(bpm);
+        }
     };
 
     const onDelaySecondsChanged = (newDelaySeconds: string) => {
+        if (!isValidDelaySeconds(newDelaySeconds)) {
+            setIsValid(false);
+            setDelaySecondsStr(newDelaySeconds);
+            return;
+        }
+
         const delaySeconds = Number(newDelaySeconds);
         const delayMs = delaySeconds * 1000;
         setDelaySecondsStr(newDelaySeconds);
 
         const newBpm = convertDelayMsToBpm(delayMs);
+        setIsValid(true);
         setBpmStr("" + newBpm);
-        onTempoChanged(newBpm);
+
+        if (isValid) {
+            onTempoChanged(newBpm);
+        }
     };
 
 
